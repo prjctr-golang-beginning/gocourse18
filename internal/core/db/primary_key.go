a8dbcfc7f01e7a88d94806f9dd8cd2dfd214120a
maks <m.morozov@autodoc.eu>
2024-02-05 18:44:09 +0200

    delete excess

--- internal/core/db/primary_key.go
+++ internal/core/db/primary_key.go
@@ -8,15 +8,8 @@ import (
 type PKType string
 
 const (
-	PkTypeID      PKType = "ID"
-	PkTypeComplex PKType = "Complex"
-
 	KeyID string = "id"
 )
-
-type PrimaryKeyable interface {
-	PrimaryKey() PrimaryKey
-}
 
 type PrimaryKey interface {
 	UUID() (uuid.UUID, bool)
@@ -32,7 +25,6 @@ func NewPrimaryKey[S PrimaryKeySrc](key S) PrimaryKey {
 	switch val := any(key).(type) {
 	case int, int64, string, uuid.UUID:
 		return &primaryKey{
-			tp:      PkTypeID,
 			key:     map[string]any{KeyID: val},
 			_fields: []string{KeyID},
 		}
@@ -42,10 +34,8 @@ func NewPrimaryKey[S PrimaryKeySrc](key S) PrimaryKey {
 }
 
 type primaryKey struct {
-	tp      PKType
 	key     sq.Eq
 	_fields []string
-	_values []any
 }
 
 func (s *primaryKey) Fields() []string {
